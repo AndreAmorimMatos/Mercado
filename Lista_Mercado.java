3ad103be271ad2cbe71d8ce8857474e2cc85073d
André Matos <amatos497@gmail.com>
2016-05-20 10:55:10 -0300

    commit

---
+++ Lista_Mercado.java
@@ -0,0 +1,26 @@
+package br.com.caelum.tarefas;
+public class Lista_Mercado {
+	private int cod_lista; 
+	private String item;
+	private double preco;
+	
+	public int getCod_lista() {
+		return cod_lista;
+	}
+	public void setCod_lista(int cod_lista) {
+		this.cod_lista = cod_lista;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+}
